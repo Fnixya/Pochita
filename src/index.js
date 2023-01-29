339ef761aa2bef959bb87b717c8e800033b6049b
Fnixya <62068603+Fnixya@users.noreply.github.com>
2023-01-29 19:28:18 +0100

    Pochita router path change

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Pochita from './components/three/Pochita';
 
 const router = createHashRouter([
   {
-    path: "/Pochita/",
+    path: "",
     element: <Pochita />,
     // errorElement: 
     // loader: rootLoader, // pantalla de carga
